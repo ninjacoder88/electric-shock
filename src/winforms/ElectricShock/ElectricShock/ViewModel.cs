dbeef8eddb12c43cabe803f551416fe23d302fc3
tomharris <tomharris@idtdna.com>
2019-02-07 15:42:22 -0600

    Added ApplicationModel

--- src/winforms/ElectricShock/ElectricShock/ViewModel.cs
+++ src/winforms/ElectricShock/ElectricShock/ViewModel.cs
@@ -1,8 +1,12 @@
 ﻿using System.ComponentModel;
+using ElectricShock.Models;
 
 namespace ElectricShock
 {
     public class ViewModel
     {
+        public BindingList<ApplicationModel> Applications => _applicationModels ?? (_applicationModels = new BindingList<ApplicationModel>());
+
+        private BindingList<ApplicationModel> _applicationModels;
     }
 }
