ca09f857593540871e1c20c793197c7d69a38991
tomharris <tomharris@idtdna.com>
2019-02-07 15:37:23 -0600

    Added new solution and project

---
+++ src/winforms/ElectricShock/ElectricShock/Form1.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Forms;
+
+namespace ElectricShock
+{
+    public partial class Form1 : Form
+    {
+        public Form1(ViewModel viewModel)
+        {
+            _viewModel = viewModel;
+            InitializeComponent();
+        }
+
+        private readonly ViewModel _viewModel;
+    }
+}
